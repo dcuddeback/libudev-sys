5efe989146cc6e492e58bcdee43d486554e3a7e8
David Cuddeback <david.cuddeback@gmail.com>
2018-01-07 13:07:13 -0700

    [fix #2] write temp files to OUT_DIR

--- build.rs
+++ build.rs
@@ -1,19 +1,18 @@
 extern crate pkg_config;
 
-use std::fs::{self,File};
+use std::env;
+use std::fs::File;
 use std::path::Path;
 use std::process::Command;
 
 use std::io::prelude::*;
 
 fn check_func(function_name: &str) -> bool {
-    let test_file_path = format!("target/build/check_{}.rs", function_name);
-    let test_file_name = Path::new(&test_file_path[..]);
-
-    fs::create_dir_all("target/build").unwrap();
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let test_file_name = Path::new(&out_dir).join(format!("check_{}.rs", function_name));
 
     {
-        let mut test_file = File::create(test_file_name).unwrap();
+        let mut test_file = File::create(&test_file_name).unwrap();
 
         writeln!(&mut test_file, "extern \"C\" {{").unwrap();
         writeln!(&mut test_file, "    fn {}();", function_name).unwrap();
@@ -27,8 +26,8 @@ fn check_func(function_name: &str) -> bool {
     }
 
     let output = Command::new("rustc").
-        arg(test_file_name).
-        arg("--out-dir").arg("target/build/").
+        arg(&test_file_name).
+        arg("--out-dir").arg(&out_dir).
         arg("-l").arg("udev").
         output().unwrap();
 
